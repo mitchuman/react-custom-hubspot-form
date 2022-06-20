cd8cc66ed91cb56891fc67524f565ee834353bab
Mitchell Christ <mitchell@human.marketing>
2022-06-20 10:55:57 -0700

    add external and terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,19 @@
 import pkg from './package.json'
+import external from 'rollup-plugin-peer-deps-external'
 import babel from '@rollup/plugin-babel'
-import { terser } from "rollup-plugin-terser"
+import { terser } from 'rollup-plugin-terser'
 
 export default {
 	input: 'src/index.jsx',
 	output: [
-		{
-			file: pkg.main,
-			format: 'esm',
-			exports: 'named',
-			sourcemap: true,
-			strict: false
-		}
+		{ file: pkg.module, format: 'esm', }
 	],
 	plugins: [
+		external(),
 		babel({
-			presets: ['@babel/preset-react'],
+			presets: [
+				['@babel/preset-react', { runtime: 'automatic' }],
+			],
 		}),
 		terser(),
 	],
