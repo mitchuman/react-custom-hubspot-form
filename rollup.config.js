c6b2eb938c750a745a843382c623ab679065c795
Mitchell Christ <mitchell@human.marketing>
2022-06-20 10:47:14 -0700

    use rollup

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import pkg from './package.json'
+import babel from '@rollup/plugin-babel'
+import { terser } from "rollup-plugin-terser"
+
+export default {
+	input: 'src/index.jsx',
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs',
+			exports: 'named',
+			sourcemap: true,
+			strict: false
+		}
+	],
+	plugins: [
+		babel({
+			presets: ['@babel/preset-react'],
+		}),
+		terser(),
+	],
+	external: ['react', 'react-dom']
+}
